d7e904febaefe3771372c1e32c75690eb062d75f
vanes <vanessacamahuali28@gmail.com>
2023-05-02 22:53:27 -0300

    criando area texto

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { GlobalStyle } from "./styles";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+    <GlobalStyle />
+  </React.StrictMode>
+);
